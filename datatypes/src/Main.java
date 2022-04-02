4c057c1c7ce6fa8e55111db4770c7bdd2b07046b
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:31:17 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// java tip güvenli bir dildir
+        int sayi = 222;
+        double sayi1 = 0.5;
+        float sayi2 = 22;
+        long sayi3=222222222;
+        char karakter ='a';
+        boolean dogruMu=false;
+        String sehir = "Ankara";
+
+
+    }
+}
