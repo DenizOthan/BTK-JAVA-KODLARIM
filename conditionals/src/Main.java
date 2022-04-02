373107a7ec7504621a13e3331eee6e145e2550a1
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:31:06 +0300

    Add files via upload

---
+++ conditionals/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int sayi = 24;
+        if(sayi<15){
+            System.out.println("Sayı 15den küçüktür.");
+        }
+
+
+        if(sayi<20){
+            System.out.println("Sayı 20den küçüktür.");
+        }else if(sayi==20){
+            System.out.println("Sayı 20 ye eşittir");
+
+        }else{
+            System.out.println("sayı 20 den büyüktür.");
+        }
+    }
+}
