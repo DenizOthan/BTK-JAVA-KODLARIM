e5c8bd6ce2f92e8ed18a3d446a72c2fcd8f01702
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:36:55 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //caseSensetive
+        //camelCase
+        int sayi = 10;
+        int sayi2 = 20;
+        String mesaj ="Öğrenci sayısı  : ";
+        System.out.println(mesaj +sayi);
+        System.out.println(mesaj +sayi2);
+
+
+
+
+
+    }
+}
