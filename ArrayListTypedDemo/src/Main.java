b61e7a68b3e64fba22955e21aeb5c538e570a6ec
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:29:00 +0300

    Add files via upload

---
+++ ArrayListTypedDemo/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> sehirler = new ArrayList<String>();
+        sehirler.add("Ankara");
+        sehirler.add("İstanbul");
+        sehirler.add("İzmir");
+        sehirler.remove("İstanbul");
+        for (String sehir:sehirler){
+            System.out.println(sehir);
+        }
+    }
+}
