894b963b04f752b9de45e2de9c90109028174290
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:36:12 +0300

    Add files via upload

---
+++ stringsdemos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	String mesaj = "Bugün Hava Çok Güzel";
+    String mesaj2 =mesaj.concat(" Yaşasın");
+
+    System.out.println(mesaj);
+    System.out.println("Eleman Sayısı : "+mesaj.length());
+    System.out.println("5.eleman sayısı " +mesaj.charAt(4));
+    System.out.println(mesaj.concat(" Yaşasın"));
+    System.out.println(mesaj2);
+
+    }
+}
