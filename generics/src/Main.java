506180467491b98faa2ea2ea81713c29115445df
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2022-04-02 13:31:48 +0300

    Add files via upload

---
+++ generics/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList<String > sehirler = new ArrayList();
+        sehirler.add("Ankara");
+        sehirler.add("İstanbul");
+        sehirler.add("İzmir");
+    }
+}
---
+++ generics/src/MyList.java
@@ -0,0 +1,10 @@
+public class MyList <T>{
+    public void Add(T value){
+
+    }
+    public void remove(T value){
+        MyList<Customer> sehirler = new MyList<Customer>();
+        sehirler.Add(new Customer());
+        sehirler.Add(new Customer());
+    }
+}
